83af8ef64d1810a1fc480319a1771f301125ef6f
boben195 <boben195@ukr.net>
2023-12-17 19:42:39 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,15 @@
+const form = document.querySelector('.login-form');
+const buttom = document.querySelector('button');
 
+form.addEventListener('submit', evn => {
+    evn.preventDefault();
+    const info = {
+        email: evn.target.email.value,
+        password: evn.target.password.value,
+    }
+    console.log(info);
+    if (evn.target.email.value === '' || evn.target.password.value === '') {
+        alert('All form fields must be filled in');
+    }
+    form.reset()
+})
