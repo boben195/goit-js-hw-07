fab2376eddf621d04cd82458ac2ec4700ad549e7
boben195 <boben195@ukr.net>
2023-12-17 20:33:05 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const form = document.querySelector('input');
+const buttonCr = document.querySelector('[data-create]');
+const buttonDes = document.querySelector('[data-destroy]');
+const div = document.querySelector('#boxes');
+
+
+function destroyBoxes() {
+  div.textContent = '';
+}
+
+
+function createBoxes(amount) {
+  destroyBoxes();
+  for (let i = 0; i < amount; i++) {
+    let boxe = 30 + 10 * i;
+    const divBox = document.createElement('div');
+    divBox.style.cssText = `width: ${boxe}px; height: ${boxe}px; background-color: ${getRandomHexColor()}`
+    div.insertAdjacentElement('beforeend', divBox)
+  }
+  form.value = ''
+}
+
+
+buttonCr.addEventListener('click', () => {
+  if (1 <= form.value || form.value <= 100) {
+    createBoxes(form.value)
+  }
+})
+
+
+buttonDes.addEventListener('click', destroyBoxes)
