ddd1f0b8881b2c35f1595ef1be12e2e585244ce9
boben195 <boben195@ukr.net>
2023-12-17 19:57:02 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,14 @@
+const button = document.querySelector('.change-color');
+const body = document.querySelector('body');
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+button.addEventListener('click', evn => {
+  body.style.backgroundColor = getRandomHexColor()
+})
